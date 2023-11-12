0d1c214dd35333b1a0ccaee9e9a8c368ae253e5b
Alasdair-Reavey <alasdairreavey@gmail.com>
2023-11-12 19:14:07 +0000

    [ADD]Player scene

--- Scripts/Entities/Player.gd
+++ Scripts/Entities/Player.gd
@@ -4,7 +4,7 @@ const ACCELERATION = 75.0
 const SPEED = 150.0
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Get the input horizontalDirection and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
