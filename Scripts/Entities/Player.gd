951e2648b4514e88f5e538b854c44102bf73733e
Alasdair-Reavey <alasdairreavey@gmail.com>
2023-11-12 15:09:03 +0000

    [MOD]Movement vector is now normalised

--- Scripts/Entities/Player.gd
+++ Scripts/Entities/Player.gd
@@ -1,23 +1,12 @@
 extends CharacterBody2D
 
-
+const ACCELERATION = 75.0
 const SPEED = 150.0
 
 
 func _physics_process(delta):
 	# Get the input horizontalDirection and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var horizontalDirection = Input.get_axis("ui_left", "ui_right")
-	var verticalDirection = Input.get_axis("ui_up", "ui_down")
-	
-	if horizontalDirection:
-		velocity.x = horizontalDirection * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if verticalDirection:
-		velocity.y = verticalDirection * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = velocity.move_toward(input_direction * SPEED, ACCELERATION)
 	move_and_slide()
